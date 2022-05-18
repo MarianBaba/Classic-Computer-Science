c25ed0aac9c6753ff40635884263c25dd3b29dd6
MarianBaba <marianbaba16@gmail.com>
2022-05-18 14:30:51 +0200

    fibonacci with memoization technique

--- src/Simplest/Fibonacci.java
+++ src/Simplest/Fibonacci.java
@@ -1,5 +1,9 @@
 package Simplest;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 public class Fibonacci {
 	
 	//Fibonacci sequence = element n is the sum o n - 1 and n - 2, except for the first two elements: 1 and 0
@@ -16,7 +20,26 @@ public class Fibonacci {
 	
 	//let's try using the memoization technique: the program remembers what has already computed and doesn't need to compute it
 	//multiple times
+	
+	//we create a map for storing all the values already computed: the first two are given
+	//now fibonacci(20) only need around 40 calls
+	static Map<Integer, Integer> memo = new HashMap<>(Map.of(0, 0, 1, 1)); 
+	
+	private static int fibonacciMemoization(int n) {
+		if (!memo.containsKey(n)) { //the algorithm computes the nth fibonacci value only if it needs to
+			memo.put(n, fibonacciMemoization(n-2) + fibonacciMemoization(n-1));
+		}
+		return memo.get(n);
+	}
+	
+	//counterintuitively, the most efficient way of computed a value of the fibonacci sequence is iteratively and not recursively
+
+	
 	private int last = 0, next = 1; //first two elements of the fibonacci sequence aka base cases
+	
+//	public IntStream stream() {
+//		
+//	}
 	
 	public static void main(String... strings) {
 		System.out.println(fibonacciRecursive(5));
