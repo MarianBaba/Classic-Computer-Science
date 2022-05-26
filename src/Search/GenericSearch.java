2f9111adf5023417c4103b69e945c6a782e6a8ce
MarianBaba <marianbaba16@gmail.com>
2022-05-26 10:58:22 +0200

    dfs implementation

--- src/Search/GenericSearch.java
+++ src/Search/GenericSearch.java
@@ -1,6 +1,11 @@
 package Search;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class GenericSearch {
 
@@ -32,33 +37,63 @@ public class GenericSearch {
         return false;
     }
 
-    //node class
+    // node class
     public static class Node<T> implements Comparable<Node<T>> {
-		
-		final T state;
-		Node<T> parent;
-		double cost;
-		double heuristic;
-		
-		Node(T state, Node<T> parent) {
-			this.state = state;
-			this.parent = parent;
-		}
-		
-		Node(T state, Node<T> parent, double cost, double heuristic) {
-			this.state = state;
-			this.parent = parent;
-			this.cost = cost;
-			this.heuristic = heuristic;
-		}
-		
-		@Override
-		public int compareTo(Node<T> other) {
-			Double mine = cost + heuristic;
-			Double theirs = other.cost + other.heuristic;
-			return mine.compareTo(theirs);
-		}
-	}
+
+        final T state;
+        Node<T> parent;
+        double cost;
+        double heuristic;
+
+        Node(T state, Node<T> parent) {
+            this.state = state;
+            this.parent = parent;
+        }
+
+        Node(T state, Node<T> parent, double cost, double heuristic) {
+            this.state = state;
+            this.parent = parent;
+            this.cost = cost;
+            this.heuristic = heuristic;
+        }
+
+        @Override
+        public int compareTo(Node<T> other) {
+            Double mine = cost + heuristic;
+            Double theirs = other.cost + other.heuristic;
+            return mine.compareTo(theirs);
+        }
+    }
+
+    public static <T> Node<T> dfs(T initial, Predicate<T> goalTest, Function<T, List<T>> successors) {
+
+        // frontier is where we've yet to go
+        Stack<Node<T>> frontier = new Stack<>();
+        frontier.push(new Node<>(initial, null));
+
+        // explored is where we've benn
+        Set<T> explored = new HashSet<>();
+        explored.add(initial);
+
+        // keep going while there is more to explore
+        while (!frontier.isEmpty()) {
+            Node<T> currentNode = frontier.pop();
+            T currentState = currentNode.state;
+            // if we found the goal, we return it
+            if (goalTest.test(currentState)) {
+                return currentNode;
+            }
+            // check where we can go next and haven't explored
+            for (T child : successors.apply(currentState)) {
+                if (explored.contains(child)) {
+                    continue; // skip children we explored
+                }
+                explored.add(child);
+                frontier.push(new Node<>(child, currentNode));
+            }
+        }
+        return null; // went through everything and never found goal
+    }
 
     public static void main(String... strings) {
         System.out.println(linearContains(List.of(1, 5, 15, 15, 15, 15, 20), 5));
