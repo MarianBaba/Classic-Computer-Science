ca2b35778c20a3573f94c0c532b9c63713b818d5
MarianBaba <marianbaba16@gmail.com>
2022-05-27 19:26:15 +0200

    finished impementing the missionaries & cannibals problem with bfs

--- src/Search/Missionaries.java
+++ src/Search/Missionaries.java
@@ -1,5 +1,12 @@
 package Search;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import Search.GenericSearch.Node;
+
 public class Missionaries {
     private static final int MAX_NUM = 3;
     private final int wm;
@@ -37,6 +44,105 @@ public class Missionaries {
             return false;
         }
         return true;
+    }
+
+    public static List<Missionaries> successors(Missionaries mcs) {
+        List<Missionaries> sucs = new ArrayList<>();
+
+        if (mcs.boat) { // boat on west bank
+
+            if (mcs.wm > 1) {
+                sucs.add(new Missionaries(mcs.wm - 2, mcs.wc, !mcs.boat));
+            }
+
+            if (mcs.wm > 0) {
+                sucs.add(new Missionaries(mcs.wm - 1, mcs.wc, !mcs.boat));
+            }
+
+            if (mcs.wc > 1) {
+                sucs.add(new Missionaries(mcs.wm, mcs.wc - 2, !mcs.boat));
+            }
+
+            if (mcs.wc > 0) {
+                sucs.add(new Missionaries(mcs.wm, mcs.wc - 1, !mcs.boat));
+            }
+
+            if (mcs.wc > 0 && mcs.wm > 0) {
+                sucs.add(new Missionaries(mcs.wm - 1, mcs.wc - 1, !mcs.boat));
+            }
+        } else { // boat on east bank
+
+            if (mcs.em > 1) {
+                sucs.add(new Missionaries(mcs.wm + 2, mcs.wc, !mcs.boat));
+            }
+
+            if (mcs.em > 0) {
+                sucs.add(new Missionaries(mcs.wm + 1, mcs.wc, !mcs.boat));
+            }
+
+            if (mcs.ec > 1) {
+                sucs.add(new Missionaries(mcs.wm, mcs.wc + 2, !mcs.boat));
+            }
+
+            if (mcs.ec > 0) {
+                sucs.add(new Missionaries(mcs.wm, mcs.wc + 1, !mcs.boat));
+            }
+
+            if (mcs.ec > 0 && mcs.em > 0) {
+                sucs.add(new Missionaries(mcs.wm + 1, mcs.wc + 1, !mcs.boat));
+            }
+        }
+        sucs.removeIf(Predicate.not(Missionaries::isLegal));
+        return sucs;
+    }
+
+    public static void displaySolution(List<Missionaries> path) {
+        if (path.size() == 0) { // sanity check
+            return;
+        }
+        Missionaries oldState = path.get(0);
+        System.out.println(oldState);
+        for (Missionaries currentState : path.subList(1, path.size())) {
+            if (currentState.boat) {
+                System.out.printf("%d missionaries and %d cannibals moved from the east bank to the west bank.%n",
+                        oldState.em - currentState.em, oldState.ec - currentState.ec);
+            } else {
+                System.out.printf("%d missionaries and %d cannibals moved from the west bank to the east bank.%n",
+                        oldState.wm - currentState.wm,
+                        oldState.wc - currentState.wc);
+            }
+            System.out.println(currentState);
+            oldState = currentState;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(boat, ec, em, wc, wm);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Missionaries other = (Missionaries) obj;
+        return boat == other.boat && ec == other.ec && em == other.em && wc == other.wc && wm == other.wm;
+    }
+
+    public static void main(String... strings) {
+        Missionaries start = new Missionaries(MAX_NUM, MAX_NUM, true);
+        Node<Missionaries> solution = GenericSearch.bfs(start, Missionaries::goalTest, Missionaries::successors);
+
+        if (solution == null) {
+            System.out.println("No solution found");
+        } else {
+            List<Missionaries> path = GenericSearch.nodeToPath(solution);
+            displaySolution(path);
+        }
     }
 
 }
