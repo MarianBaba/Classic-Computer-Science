f2e2da68650ced3b7ffd82447bee3fe05218af9d
MarianBaba <marianbaba16@gmail.com>
2022-05-30 22:36:23 +0200

    defined edge class

---
+++ src/Graphs/Edge.java
@@ -0,0 +1,20 @@
+package Graphs;
+
+public class Edge {
+    public final int u;
+    public final int v;
+
+    public Edge(int u, int v) {
+        this.u = u;
+        this.v = v;
+    }
+
+    public Edge reversed() {
+        return new Edge(v, u);
+    }
+
+    @Override
+    public String toString() {
+        return u + " -> " + v;
+    }
+}
