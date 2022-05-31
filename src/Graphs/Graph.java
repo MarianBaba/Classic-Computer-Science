9cb0f0b9fc6066c296971b3ae59f62970c4056bf
MarianBaba <marianbaba16@gmail.com>
2022-05-31 22:16:22 +0200

    utility methods for Graphs

--- src/Graphs/Graph.java
+++ src/Graphs/Graph.java
@@ -1,7 +1,9 @@
 package Graphs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class Graph<V, E extends Edge> {
 
@@ -17,6 +19,58 @@ public abstract class Graph<V, E extends Edge> {
         for (V vertex : vertices) {
             edges.add(new ArrayList<>());
         }
+    }
+
+    public int getVertexCount() {
+        return vertices.size();
+    }
+
+    public int getEdgeCount() {
+        return edges.stream().mapToInt(ArrayList::size).sum();
+    }
+
+    public int addVertex(V vertex) {
+        vertices.add(vertex);
+        edges.add(new ArrayList<>());
+        return getVertexCount() - 1;
+    }
+
+    public V vertexAt(int index) {
+        return vertices.get(index);
+    }
+
+    public int indexOf(V vertex) {
+        return vertices.indexOf(vertex);
+    }
+
+    public List<V> neighborsOf(int index) {
+        return edges.get(index).stream()
+                .map(edge -> vertexAt(edge.v))
+                .collect(Collectors.toList());
+    }
+
+    public List<V> neighborsOf(V vertex) {
+        return neighborsOf(indexOf(vertex));
+    }
+
+    public List<E> edgesOf(int index) {
+        return edges.get(index);
+    }
+
+    public List<E> edgesOf(V vertex) {
+        return edgesOf(indexOf(vertex));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < getVertexCount(); i++) {
+            sb.append(vertexAt(i));
+            sb.append("->");
+            sb.append(Arrays.toString(neighborsOf(i).toArray()));
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
     }
 
 }
